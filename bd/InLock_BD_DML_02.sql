c1271a325e6e6acbf17e08726c70b09933d4ff71
Mayara Lupeti Turbiani <49393984808@info.local>
2020-03-02 17:32:02 -0300

    banco de dados finalizado

--- bd/InLock_BD_DML_02.sql
+++ bd/InLock_BD_DML_02.sql
@@ -14,7 +14,8 @@ VALUES ('Blizzard'),
 	   ('Square Enix');
 
 INSERT INTO Jogos (NomeJogo, Descricao, DataLancamento, Valor, IdEstudio)
-VALUES ('Dablo 3', 'É um jogo que contém bastante ação e é viciante, seja 
+VALUES ('Diablo 3', 'É um jogo que contém bastante ação e é viciante, seja 
         você um novato ou um fã', '15/05/2012', 99.00, 1),
 	   ('Red Dead Redemption', 'Jogo eletrônico de ação-aventura western',
 	    '26/10/2018', 120.00, 2);
+
